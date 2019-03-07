439efcc18b8f54f0f32ea835b9a76a9fb02e2e40
Mgoyco12 <43241864+Mgoyco12@users.noreply.github.com>
2019-03-07 09:34:40 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".calc").click(function(){
+    var sneakers=parseInt($(".kicks").val());
+    var math=sneakers*132;
+    var name=$(".nombre").val();
+    var kicks=$(".brand").val();
+    $("p").text();
+    console.log(sneakers,math);
+    
+    
+    
+    });
